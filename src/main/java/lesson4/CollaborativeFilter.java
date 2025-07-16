2a91eeeb9c781512c30ab0de40dfae0070e8bae0
Andrew Shin <andyrpg@gmail.com>
2025-07-16 14:28:57 -0700

    lesson 4 completed

---
+++ src/main/java/lesson4/CollaborativeFilter.java
@@ -0,0 +1,12 @@
+package lesson4;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class CollaborativeFilter implements Filter {
+    public String[] getRecommendations(String movie) {
+        return new String[] {"Finding Nemo", "Ice Age", "Toy Story"};
+    }
+}
---
+++ src/main/java/lesson4/Filter.java
@@ -0,0 +1,5 @@
+package lesson4;
+
+public interface Filter {
+    public String[] getRecommendations(String movie);
+}
