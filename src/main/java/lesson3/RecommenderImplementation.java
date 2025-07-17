408d9a47721cc680fc1a3e93cecfd8e8dc1252e8
Andrew Shin <andyrpg@gmail.com>
2025-07-17 09:50:49 -0700

    lesson5, autowiring by name

--- src/main/java/lesson3/RecommenderImplementation.java
+++ src/main/java/lesson3/RecommenderImplementation.java
@@ -8,10 +8,6 @@ public class RecommenderImplementation {
     @Autowired
     private Filter filter;
 
-    public RecommenderImplementation(Filter filter) {
-        super();
-        this.filter = filter;
-    }
 
     public String[] recommendMovies(String movie) {
         System.out.println("Name of the filter in use: " + filter + "\n");
---
+++ src/main/java/lesson5/Filter.java
@@ -0,0 +1,5 @@
+package lesson5;
+
+public interface Filter {
+    public String[] getRecommendations(String movie);
+}
